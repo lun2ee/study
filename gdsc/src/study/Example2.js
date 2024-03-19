9c9f3a8954da9a364f77e372fefc8d78a8b498a1
lun2ee <eunjin22ee@gmail.com>
2024-03-19 15:22:59 +0900

    2번째 리액트 스터디 입니다
    49쪽까지 3장 읽어보고
    class component (setstage)는 빼고
    함수형 component (hooks) 만 공부하기

---
+++ gdsc/src/study/Example2.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Example2 = () => {
+  const name = '리액트';
+
+  return <div>{name === '리액트' && <h3>리액트입니다.</h3>}</div>;
+};
+
+export default Example2;
---
+++ gdsc/src/study/FirstExample.js
@@ -0,0 +1,20 @@
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [channame, setChanegename] = useState('리액트');
+  // 상수를 변수처럼 쓰기 위해서 useState 사용
+  const onchange = (e) => {
+    setChanegename(e.target.value);
+  };
+
+  return (
+    <>
+      {channame === '리액트' ? <h1>리엑트입니다.</h1> : <h1>리엑트아님</h1>}
+
+      <input onChange={onchange} />
+    </>
+  );
+}
+
+export default App;
